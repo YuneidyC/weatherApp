c9e29099ea715e2d42d50d33360f2a2fa52786e4
YuneidyC <yuneidycastillo25@gmail.com>
2022-12-14 18:49:56 +0000

    Refactor CurrentWeather component

---
+++ src/components/CurrentWeather/index.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+
+import { Search } from "../Search";
+
+import "./styles.css";
+
+export const CurrentWeather = (props) => {
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API}?q=London,GB&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
+            .then((res) => res.json())
+            .then((res) => {
+                props.setData(res);
+                props.setWeather(res.list);
+                props.setFiveDays([]);
+            })
+            .catch((e) => console.log(e));
+        
+    }, []);
+
+    return (
+        <section className="currentWeather" >
+            {props.data &&
+                <div className="currentWeather--nav">
+                    <Search
+                        setData={props.setData}
+                        setWeather={props.setWeather}
+                        setFiveDays={props.setFiveDays}
+                    />
+                    <h1>{props.data.city.name}, {props.data.city.country}</h1>
+                </div>
+            }
+            {props.weather && props.children}
+        </section>
+    );
+};
