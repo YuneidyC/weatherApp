8bc7b9c8ef97aa4b6c40afc87faccf0b26921cb6
YuneidyC <yuneidycastillo25@gmail.com>
2022-12-14 18:52:32 +0000

    Add Day component

--- src/components/Day/index.jsx
+++ src/components/Day/index.jsx
@@ -1,24 +1,25 @@
 import React from "react";
+
 import './styles.css';
 
-export const Day = ({ weather, main, dt_txt }) => {
+export const Day = (props) => {
     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-    const iconLink = "http://openweathermap.org/img/wn/" + weather[0].icon + '@2x.png';
+    const iconLink = "http://openweathermap.org/img/wn/" + props.day.weather[0].icon + '@2x.png';
 
     return (
         <li className="day">
             <div className="day--date">
-                <p>{weekday[(new Date(dt_txt.replace(" ", "T"))).getDay()]}</p>
-                <p>{(new Date(dt_txt.replace(" ", "T"))).getDate() + " " + monthName[(new Date(dt_txt.replace(" ", "T"))).getMonth()]}</p>
+                <p>{weekday[(new Date(props.day.dt_txt.replace(" ", "T"))).getDay()]}</p>
+                <p>{(new Date(props.day.dt_txt.replace(" ", "T"))).getDate() + " " + monthName[(new Date(props.day.dt_txt.replace(" ", "T"))).getMonth()]}</p>
             </div>
             <img className="day--weather" src={iconLink} alt="" />
             <div className="temperature--container">
                 <div className="temperature--container__high">
-                    <span>{main.temp_max}°</span>
+                    <span>{props.day.main.temp_max}°</span>
                 </div>
                 <div className="temperature--container__low">
-                    <span>{main.temp_min}°</span>
+                    <span>{props.day.main.temp_min}°</span>
                 </div>
             </div>
         </li>
