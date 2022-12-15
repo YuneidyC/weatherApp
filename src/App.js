511dc2e0d10fef69e5f398f50aff7f3153b51e21
YuneidyC <yuneidycastillo25@gmail.com>
2022-12-15 14:39:37 +0000

    Add swith case statement
    With this component we can change the background-image depending
    on the city's weather

--- src/App.js
+++ src/App.js
@@ -8,10 +8,24 @@ import { Day } from './components/Day';
 import { ListDays } from './components/listDays';
 import { CurrentWeather } from './components/CurrentWeather';
 
+import cloudsDay from './assets/images/clouds.jpg';
+import cloudsNight from './assets/images/cloudsNight.jpg';
+import showerRain from './assets/images/showerRain.jpg'
+import rain from './assets/images/rain.jpg'
+import mist from './assets/images/mist.jpg'
+import drizzle from './assets/images/drizzle.jpg'
+import snowDay from './assets/images/snowDay.jpg';
+import snowNight from './assets/images/snowNight.jpg';
+import thunderstorm from './assets/images/thunderstorm.jpg';
+import clearSkyDay from './assets/images/clearSkyDay.jpg';
+import clearSkyNight from './assets/images/clearSkyNight.jpg';
+
+
 function App() {
     const [weather, setWeather] = useState([]);
     const [data, setData] = useState('');
     const [fiveDays, setFiveDays] = useState([]);
+
     const { coords, isGeolocationEnabled } = useGeolocated({
         positionOptions: {
             enableHighAccuracy: false,
@@ -19,18 +33,78 @@ function App() {
         userDecisionTimeout: 5000,
     });
 
+    function SwitchCase(props) {
+        const background = document.getElementsByClassName('currentWeather')[0];
+        const body = document.getElementsByTagName('body')[0];
+    
+        switch (props.value[0].weather[0].icon) {
+            case '01d':
+                background.style.backgroundImage = `url(${clearSkyDay})`;
+                break;
+            case '01n':
+                background.style.backgroundImage = `url(${clearSkyNight})`;
+                break;
+            case '02d':
+            case '03d':
+            case '04d':
+                background.style.backgroundImage = `url(${cloudsDay})`;
+                break;
+            case '02n':
+            case '03n':
+            case '04n':
+                background.style.backgroundImage = `url(${cloudsNight})`;
+                break;
+            case '300':
+            case '301':
+            case '302':
+            case '310':
+            case '311':
+            case '312':
+            case '313':
+            case '314':
+            case '321':
+                background.style.backgroundImage = `url(${drizzle})`;
+                break;
+            case '50d':
+                background.style.backgroundImage = `url(${mist})`;
+                break;
+            case '09d':
+                background.style.backgroundImage = `url(${showerRain})`;
+                break;
+            case '10d':
+                background.style.backgroundImage = `url(${rain})`;
+                break;
+            case '11d':
+                background.style.backgroundImage = `url(${thunderstorm})`;
+                break;
+            case '13d':
+                background.style.backgroundImage = `url(${snowDay})`;
+                body.style.color = 'black';
+                break;
+            case '13n':
+                background.style.backgroundImage = `url(${snowNight})`;
+                break;
+            default:
+                background.style.backgroundColor = "dimgray";
+                break;
+        }
+    }
+
     return !isGeolocationEnabled ? (
         <CurrentWeather
             weather={weather}
             setWeather={setWeather}
             data={data}
             setData={setData}
-            setFiveDays={setFiveDays}>
+            setFiveDays={setFiveDays}
+            SwitchCase={SwitchCase}>
             {weather.length && (
                 <div className="Weather-App">
+                    <SwitchCase value={weather} />
                     <DetailsWeather
                         data={data}
-                        weather={weather}></DetailsWeather>
+                        weather={weather}>
+                    </DetailsWeather>
                     <ListDays
                         weather={weather}
                         fiveDays={fiveDays}
@@ -50,12 +124,15 @@ function App() {
             setWeather={setWeather}
             data={data}
             setData={setData}
-            setFiveDays={setFiveDays}>
+            setFiveDays={setFiveDays}
+            SwitchCase={SwitchCase}>
             {weather.length && (
                 <div className="Weather-App">
+                    <SwitchCase value={weather} />
                     <DetailsWeather
                         data={data}
-                        weather={weather}></DetailsWeather>
+                        weather={weather}>
+                    </DetailsWeather>
                     <ListDays
                         weather={weather}
                         fiveDays={fiveDays}
